cb15e7fb56eca3f8acdf74435c023abd9a894de7
DUDAMARINI <127321124+DUDAMARINI@users.noreply.github.com>
2023-11-08 11:44:18 -0300

    ok

---
+++ main.js
@@ -0,0 +1,12 @@
+function tocaSomAplausos(){
+    document.querySelector ('#som_tecla_aplausos').play();
+}
+function tocaSomVaia(){
+    document.querySelector ('#som_tecla_vaia').play();
+}
+function tocaSomVaia(){ 
+    document.querySelector
+}
+ function tocaSomGrilo(){
+   
+ }
